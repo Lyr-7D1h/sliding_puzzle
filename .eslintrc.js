147ddb82a33387ffdc7fe69c026df62b2e950036
Lyr-7D1h <lyr-7d1h@pm.me>
2020-12-15 18:47:31 +0100

    Fix eslint for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: ["airbnb", "prettier", "prettier/react"],
   parserOptions: {
@@ -11,7 +12,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "prettier"],
+  plugins: ["react", "jest", "prettier"],
   rules: {
     "prettier/prettier": ["error"],
   },
