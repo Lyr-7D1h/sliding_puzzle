754a3d0a9ea0bb8182e4525d4c69cd87cb12d641
Lyr-7D1h <lyr-7d1h@pm.me>
2020-12-15 16:40:23 +0100

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PuzzlePage from "./features/puzzle/PuzzlePage";
+
+function App() {
+  return (
+    <div>
+      <PuzzlePage />
+    </div>
+  );
+}
+
+export default App;
